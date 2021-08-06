79b217da5b119904b9c28e7e30589e7bafb4b62d
fapjsc <p82szh@gmail.com>
2021-08-06 15:58:36 +0800

    create fetch all, fetch single, edit, delete streams action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 // Api
 import streams from '../apis/streams';
 
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAM_LIST, FETCH_STREAM_SINGLE, UPDATE_STREAM, DELETE_STREAM } from './types';
 
 export const trySignIn = userId => {
   return {
@@ -17,5 +17,26 @@ export const trySignOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreamList = () => async dispatch => {
+  const response = await streams.get('/streams');
+  dispatch({ type: FETCH_STREAM_LIST, payload: response.data });
+};
+
+export const fetchStreamSingle = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM_SINGLE, payload: response.data });
+};
+
+export const updateStream = id => async dispatch => {
+  const response = await streams.put(`/streams/${id}`);
+  dispatch({ type: UPDATE_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,10 @@
+// Auth
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+
+// Streams
+export const CREATE_STREAM = 'CREATE_STREAM';
+export const FETCH_STREAM_LIST = 'FETCH_STREAM_LIST';
+export const FETCH_STREAM_SINGLE = 'FETCH_STREAM_SINGLE';
+export const UPDATE_STREAM = 'UPDATE_STREAM';
+export const DELETE_STREAM = 'DELETE_STREAM';
