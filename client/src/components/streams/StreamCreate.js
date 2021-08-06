b29e45fe6c416ab22e8937ae1bfd9bf63550e427
fapjsc <p82szh@gmail.com>
2021-08-06 14:33:01 +0800

    redux-form sync validation

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,23 +1,49 @@
 import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = props => {
-  // Handler Function
-  const handleSubmit = formValue => {
-    console.log(formValue);
-  };
+// Handle Validate
+const validate = formValue => {
+  const { title, description } = formValue;
+  let errors = {};
+  if (!title) errors.title = 'You must enter a title';
+  if (!description) errors.description = 'You must enter description';
 
-  // Render Elements
-  const inputEl = ({ input, label }) => (
-    <div className="field">
+  return errors;
+};
+
+// Handle Submit
+const handleSubmit = formValue => {
+  console.log(formValue);
+};
+
+// Handle Error
+const handleError = ({ error, touched }) => {
+  if (touched && error) {
+    return (
+      <div className="ui error message">
+        <div className="header">{error}</div>
+      </div>
+    );
+  }
+};
+
+// Render Elements
+const renderField = ({ input, label, meta }) => {
+  const { touched, error } = meta;
+  const classes = `field ${touched && error && 'error'}`;
+  return (
+    <div className={classes}>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} autoComplete="off" />
+      {handleError(meta)}
     </div>
   );
+};
 
+const StreamCreate = props => {
   return (
-    <form onSubmit={props.handleSubmit(handleSubmit)} className="ui form">
-      <Field name="title" component={inputEl} label="Enter Title" />
-      <Field name="description" component={inputEl} label="Enter Description" />
+    <form onSubmit={props.handleSubmit(handleSubmit)} className="ui form error">
+      <Field name="title" component={renderField} label="Enter Title" />
+      <Field name="description" component={renderField} label="Enter Description" />
       <button className="ui button primary">Submit</button>
     </form>
   );
@@ -25,4 +51,5 @@ const StreamCreate = props => {
 
 export default reduxForm({
   form: 'streamCreate',
+  validate,
 })(StreamCreate);
